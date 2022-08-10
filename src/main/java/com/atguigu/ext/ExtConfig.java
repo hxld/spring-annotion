0ad7833b6e790c7071dfc533cc24391791207705
hxld <hxld88888@163.com>
2022-08-10 10:06:48 +0800

    nine commit

---
+++ src/main/java/com/atguigu/ext/ExtConfig.java
@@ -0,0 +1,12 @@
+package com.atguigu.ext;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hxld
+ * @create 2022-08-08 17:06
+ */
+@Configuration
+public class ExtConfig {
+
+}
